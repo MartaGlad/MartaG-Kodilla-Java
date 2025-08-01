408f13dde831011c6a51433049a88ea9ad01db99
Marta Gladysz <kicia.nunus@gmail.com>
2025-08-02 00:04:13 +0200

    6.2 Added user and calculator package with tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(10, 20);
+        int subResult = calculator.subtract(10, 20);
+        if (addResult == 30) {
+            System.out.println("Calculator add test OK");
+        } else {
+            System.out.println("Calculator add test has error!");
+        }
+
+        if (subResult == -10) {
+            System.out.println("Calculator sub test OK");
+        } else {
+            System.out.println("Calculator sub test has error!");
+        }
     }
 }
