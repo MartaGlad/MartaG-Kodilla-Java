50a9413a57118bb374f98618eb17700637515dc2
Marta Gladysz <kicia.nunus@gmail.com>
2025-11-11 15:32:31 +0100

    Added bubbleSort package

---
+++ kodilla-additional/src/main/java/com/kodilla/additional/bubbleSort/BubbleSort.java
@@ -0,0 +1,19 @@
+package com.kodilla.additional.bubbleSort;
+
+import java.util.List;
+
+public class BubbleSort {
+    public static List<Integer> sort(List<Integer> list) {
+        int tmp;
+        for(int i = 0; i < list.size(); i++) {
+            for(int j = i+1; j < list.size(); j++) {
+                if(list.get(i) > list.get(j)) {
+                    tmp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, tmp);
+                }
+            }
+        }
+     return list;
+    }
+}
